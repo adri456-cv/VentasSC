d5804a09ec50bbf2558a708552a4e55c64b0aaed
adri456-cv <adrianacondori679@gmail.com>
2025-11-24 23:41:29 -0400

    Microservicio de AlmacenSC-productos

---
+++ Presentacion/Controllers/ProductoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Ventas.Presentacion.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+       
+        private readonly HttpClient _httpClient;
+        public ProductoController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        [HttpGet("Productos")]
+        public async Task<IActionResult> GetPedidosCliente()
+        {
+            string URL = "https://almacensanc-production.up.railway.app/api/Producto";
+            HttpResponseMessage respuesta = await _httpClient.GetAsync(URL);
+
+            if (!respuesta.IsSuccessStatusCode)
+            {
+                
+                return StatusCode((int)respuesta.StatusCode,
+                                  $"Error al obtener los datos del microservicio Producto: {respuesta.ReasonPhrase}");
+            }
+            string jsonRespuesta = await respuesta.Content.ReadAsStringAsync();
+            JsonElement datos = JsonSerializer.Deserialize<JsonElement>(jsonRespuesta);
+            return Ok(datos);
+        }
+    }
+}
